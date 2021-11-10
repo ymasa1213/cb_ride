2dc4d3a453a5ecabd1b8f9137ba33a2d53af4ef3
ymasa1213 <ymasa1213@yahoo.co.jp>
2021-11-10 11:12:26 +0000

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,28 @@ devise_for :users,skip: [:passwords,], controllers: {
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
+scope module: :user do
+  root to: "homes#top"
+  get "about" => "homes#about"
+
+  resources :users, only: [:show, :edit, :update]
+  resources :posts
+  resources :likes, only:[:create, :destroy]
+  get "users/unsubscribe" => "users#unsubscribe"
+  patch "users/withdraw" => "users#withdraw"
+  resources :users do
+    resource :relationships, only:[:create, :destroy]
+    get :followings, on: :member
+    get :followers, on: :member
+  end
 end
+
+namespace :admin do
+  root to:"homes#top"
+  resources :users, only:[:index, :show, :edit, :update]
+end
+
+end
+
+
