4a2763a632c78d4c19cc5afc25d88bd6a9ec8d9b
ymasa1213 <ymasa1213@yahoo.co.jp>
2021-11-11 05:17:54 +0000

    association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         has_many :posts, dependent: :destroy
+         has_many :comments, dependent: :destroy
+         has_many :likes, dependent: :destroy
+         has_many :relationships, foreign_key: :following_id
+         has_many :followings, through: :relationships, source: :follower
+         has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id
+         has_many :followers, through: :reverse_of_relationships, source: :following
+         attachment :profile_image, destroy: false
 end
