dcf906d349ff5b57e51d29ed3413e1ea1e723df0
ymasa1213 <ymasa1213@yahoo.co.jp>
2021-11-11 09:20:48 +0000

    管理者情報　ログイン後遷移先変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    case resource
+      when Admin
+      admin_users_path
+      when User
+       root_path
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
