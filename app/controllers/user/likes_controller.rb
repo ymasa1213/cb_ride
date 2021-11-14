8ae00f9ee33d0ee7537dbeeeb203d65bab10576f
ymasa1213 <ymasa1213@yahoo.co.jp>
2021-11-14 14:09:07 +0000

    非同期化

--- app/controllers/user/likes_controller.rb
+++ app/controllers/user/likes_controller.rb
@@ -1,13 +1,13 @@
 class User::LikesController < ApplicationController
   def create
-    @like = current_user.likes.create(post_id: params[:post_id])
-    redirect_back(fallback_location: root_path)
+    @post = Post.find(params[:post_id])
+    like = @post.likes.new(user_id: current_user.id)
+    like.save
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @likes = current_user.likes.find_by(post_id: @post.id)
     @likes.destroy
-    redirect_back(fallback_location: root_path)
   end
 end
