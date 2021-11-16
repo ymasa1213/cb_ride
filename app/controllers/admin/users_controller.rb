b20c96cf20bf26aa46bad7f17c468ea6a2994969
ymasa1213 <ymasa1213@yahoo.co.jp>
2021-11-16 09:37:58 +0000

    退会機能

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,32 @@
 class Admin::UsersController < ApplicationController
   def index
+    @users = User.all
   end
+
 
   def show
+    @user = User.find(params[:id])
   end
 
+
   def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice]= "会員情報を変更しました"
+      redirect_to admin_user_path
+    else
+      flash[:alert]= "会員情報の変更に失敗しました"
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :profile_image, :is_deleted)
   end
 end
