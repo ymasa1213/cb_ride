e06887e8208f714b4c2c451439c1d982f3ec1c8d
ymasa1213 <ymasa1213@yahoo.co.jp>
2021-11-20 09:56:23 +0000

    レイアウト修正

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -47,6 +47,12 @@ class User::PostsController < ApplicationController
     render 'index'
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
   private
 
   def post_params
--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -2,7 +2,7 @@ class User::UsersController < ApplicationController
   before_action :authenticate_user!
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
+    @posts = @user.posts.order(id: 'DESC')
   end
 
   def edit
