408fb894c6b7b6602227bbcbfc8cd9faddc24fa8
ymasa1213 <ymasa1213@yahoo.co.jp>
2021-11-20 10:08:15 +0000

    paramsにemail追加

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -43,6 +43,6 @@ class User::UsersController < ApplicationController
 
   private
   def user_params
-      params.require(:user).permit(:name, :profile_image)
+      params.require(:user).permit(:name, :email, :profile_image)
   end
 end
