f6b2707c32c1c4f4dbda4bfd972b94ab0905c064
ymasa1213 <ymasa1213@yahoo.co.jp>
2021-11-21 09:49:15 +0000

    アカウント制限

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -32,12 +32,9 @@ class User::SessionsController < Devise::SessionsController
   def reject_user
     @user = User.find_by(email: params[:user][:email].downcase)
     if @user
-      if (@user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == false))
-        flash[:error] = "退会済みです。"
+       (@user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == false))
+        flash[:notice] = "このアカウントは管理者によって制限されています"
         redirect_to new_user_session_path
-      end
-    else
-      flash[:error] = "必須項目を入力してください。"
     end
   end
 end
