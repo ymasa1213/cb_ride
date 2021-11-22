4e69079c55bb078474cc71b44adf101da90dbf68
ymasa1213 <ymasa1213@yahoo.co.jp>
2021-11-22 05:51:59 +0000

    kaminari

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < ApplicationController
   before_action :authenticate_admin!
   def index
-    @users = User.all
+    @users = User.page(params[:page]).per(8)
   end
 
   def show
