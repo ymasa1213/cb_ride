032e7c9a30c53260cd6c7308e2a93b297a93fce3
ymasa1213 <ymasa1213@yahoo.co.jp>
2021-11-22 10:39:39 +0000

    非同期通信

--- app/controllers/user/comments_controller.rb
+++ app/controllers/user/comments_controller.rb
@@ -5,12 +5,15 @@ class User::CommentsController < ApplicationController
     comment = current_user.comments.new(comment_params)
     comment.post_id = post.id
     comment.save
-    redirect_to post_path(post)
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
   end
 
   def destroy
-    Comment.find_by(id: params[:id]).destroy
-    redirect_to post_path(params[:post_id])
+    @post = Post.find(params[:post_id])
+    @comment =  Comment.find_by(id: params[:id], post_id: params[:post_id])
+    @comment.destroy
+    @comment = Comment.new
   end
 
    private
@@ -18,4 +21,5 @@ class User::CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:comment)
   end
+
 end
